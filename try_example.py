e4f763e461c5b4328889dd434ea67a3e09e10dea
deSpooky <phurrde@gmail.com>
2025-11-21 11:15:28 +0300

    npz task

---
+++ try_example.py
@@ -0,0 +1,15 @@
+import os
+from openai import OpenAI
+client = OpenAI(api_key='sk-052d89cfe6be4d7d815c128ec700ba00',
+                base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "spooky"},
+        {"role": "user", "content": "hello spooky!"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
